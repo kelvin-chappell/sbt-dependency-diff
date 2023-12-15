9a057d447011842c80516bf0f19578f99b220a7f
Kelvin Chappell <kelvin.chappell@theguardian.com>
2023-12-15 15:51:04 +0000

    Refactor

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-ThisBuild / version := "0.1.9-SNAPSHOT"
+ThisBuild / version := "0.1.11-SNAPSHOT"
 
 ThisBuild / organization := "org.kc"
 
--- src/main/scala/org/kc/sbtdependencydiff/ZipPlugin.scala
+++ src/main/scala/org/kc/sbtdependencydiff/ZipPlugin.scala
@@ -21,14 +21,17 @@ object ZipPlugin extends AutoPlugin {
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
     sourceZipDir := Keys.sourceDirectory.value,
     targetZipDir := Keys.target.value / "zip",
-    zip := zipTask.value
+    zip := Zip(sourceZipDir.value, targetZipDir.value, Keys.sLog.value)
   )
+}
 
-  def zipTask = Def.task {
-    val log = Keys.sLog.value
-    lazy val zip = new File(targetZipDir.value, sourceZipDir.value.getName + ".zip")
-    log.info("Zipping file...")
-    IO.zip(Path.allSubpaths(sourceZipDir.value), zip)
+object Zip {
+  def apply(sourceDir: File, targetDir: File, logger: Logger): File = {
+    val zip = new File(targetDir, sourceDir.getName + ".zip")
+    logger.info(s"Source directory: $sourceDir")
+    logger.info(s"Target file: $zip")
+    logger.info("Zipping file...")
+    IO.zip(Path.allSubpaths(sourceDir), zip)
     zip
   }
 }
